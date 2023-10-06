79574660130bcadf80b1950ee42eccfc3b8849aa
Alexey Lazarev <am_laz1@mail.ru>
2023-10-06 17:27:41 +0200

    Level sets and dataset rewritten as a class

--- ricci_regularization/SyntheticDataset.py
+++ ricci_regularization/SyntheticDataset.py
@@ -48,3 +48,51 @@ def generate_dataset(D, k, n, shift_class=0, intercl_var = 0):
     train_dataset.transform = train_transform
 
     return train_dataset
+
+# definition using class
+
+class SyntheticDataset:
+    def __init__(self, k = 3, n = 6000, d=2, D=784, shift_class=0, intercl_var = 0):
+        phi = [] #list of k ontonormal bases in k planes
+        for j in range(k):
+            # creating random planes
+            rand_vectors = torch.rand(D, d)
+            q, r = torch.qr(rand_vectors)
+            phi.append(q)
+        #phi
+
+        #creating samples from normal distributions via torch distributions
+        data = []
+        shifts = []
+        for i in range(k):
+            m = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(d) + shift_class*(i+1), torch.eye(d))
+            #m = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(2), torch.eye(2))
+            samples = m.sample(sample_shape=(n,)).T
+            
+            shift = torch.randn(D,1) * intercl_var
+            shifts.append(shift) 
+
+            samples_transformed = torch.matmul(phi[i], samples) + shift
+            data.append(samples_transformed)
+            #data.append(torch.matmul(phi[i], samples))
+        data_tensor = torch.cat(data, dim=1)
+
+        data_tensor = data_tensor.T
+        data_tensor = data_tensor.reshape(k*n, 1, D)
+
+        labels_list = []
+        for i in range(k):
+            labels_list.append(i*(torch.ones(n)))
+        labels = torch.cat(labels_list)
+
+        train_dataset = torch.utils.data.TensorDataset(data_tensor,labels)
+
+        train_transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+        ])
+
+        train_dataset.transform = train_transform
+
+        self.create = train_dataset
+        self.rotations = phi
+        self.shifts = shifts
